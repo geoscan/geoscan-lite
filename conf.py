352249527f2416d354f7db885a0cf9bac1dada4e
Igor Mazaev <i.mazaev@geoscan.aero>
2019-03-06 11:48:31 +0300

    Пусковая установка. Логотипы. Мелкие правки по документам.

--- conf.py
+++ conf.py
@@ -89,6 +89,10 @@ def setup(app):
 html_theme_options = {
     'logo_only': True,
 }
+html_logo = "_static/_images/logo.png"
+html_favicon = "_static/_images/favicon.ico"
+html_show_sphinx = False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
