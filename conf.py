0312404736f807f2f7ab3b08a944a78638f7266f
Igor Mazaev <igor.aviator.nsk@gmail.com>
2019-03-26 12:08:03 +0300

    Версия 0.9 RC

--- conf.py
+++ conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Geoscan'
 author = 'Geoscan'
 
 # The short X.Y version
-version = ''
+version = '0.9'
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = 'rc'
 
 
 # -- General configuration ---------------------------------------------------
