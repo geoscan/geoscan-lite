ed82eecda928036a4810bcb1ccbc57283a3b6396
Igor Mazaev <igor.aviator.nsk@gmail.com>
2019-04-04 15:46:02 +0300

    Правки для PDF

--- conf.py
+++ conf.py
@@ -124,15 +124,15 @@ latex_elements = {
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+     'pointsize': '11pt',
 
     # Additional stuff for the LaTeX preamble.
     #
-    # 'preamble': '',
+     'preamble': '',
 
     # Latex figure (float) alignment
     #
-    # 'figure_align': 'htbp',
+     'figure_align': 'H',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
