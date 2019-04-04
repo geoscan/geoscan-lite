60e898b80ef4b3802e6fe4b6328481937763ec2d
Igor Mazaev <igor.aviator.nsk@gmail.com>
2019-04-04 13:59:38 +0300

    Правки под PDF

--- conf.py
+++ conf.py
@@ -139,7 +139,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, '101-doc.tex', '101-doc Documentation',
+    (master_doc, '101-doc.tex', 'Геоскан Lite Руководство по эксплуатации',
      'Geoscan', 'manual'),
 ]
 
